87fef077ad5288c3309c67ddd9642583cda8bdb0
Daniel <zrelick@gmail.com>
2024-04-03 16:06:42 +0200

    shrink to fit yes

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
     <html lang={locale}>
       <Head>
         {/* Include the viewport meta tag within the Head component */}
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes" />
       </Head>
 
       <FirebaseInit>
